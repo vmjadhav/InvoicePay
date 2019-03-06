43ba3bed8e7db077dcf5c42686f5c340586b50e2
Heerain <hiren8890@gmail.com>
2019-03-06 22:57:12 +0530

    add buttons on mainactivity and added screen InvoiceOption

---
+++ app/src/main/java/com/poc/invoicepay/InvoiceOptionsActivity.java
@@ -0,0 +1,13 @@
+package com.poc.invoicepay;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InvoiceOptionsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_invoice_options);
+    }
+}
--- app/src/main/java/com/poc/invoicepay/MainActivity.java
+++ app/src/main/java/com/poc/invoicepay/MainActivity.java
@@ -1,7 +1,10 @@
 package com.poc.invoicepay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +12,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button mInvoiceButton = findViewById(R.id.createInvoiceButton);
+
+        mInvoiceButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),InvoiceOptionsActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
 }
